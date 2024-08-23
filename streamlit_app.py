0660051f6c1b496b63a452fa63ead8388708e14a
ibas662 <max.pen.662@gmail.com>
2024-08-23 19:09:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 
 st.title("🎈 pred")
+
 f1=st.number_input('f1',min_value=1,max_value=10)
 f2=st.number_input('f2',min_value=1,max_value=10)
 f3=st.number_input('f3',min_value=1,max_value=100)
@@ -10,5 +11,5 @@ f3=st.number_input('f3',min_value=1,max_value=100)
 
 with open('model.pkl','rb') as file :
   model = pickle.load(file)
-var = model.predict([[engine_size, cylinder, fuelconsumption]])
+var = model.predict([[f1, f2, f3]])
 st.write('co2 pred',var[0][0])
