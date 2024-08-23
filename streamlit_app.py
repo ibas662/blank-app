0a82945fc1bfd76b0f7eecd4c78a60e57ab462f8
ibas662 <max.pen.662@gmail.com>
2024-08-23 18:47:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 pred")
-st.number_input('f2',min_value=1,max_value=10)
 st.number_input('f1',min_value=1,max_value=10)
+st.number_input('f2',min_value=1,max_value=10)
 st.number_input('f3',min_value=1,max_value=100)
