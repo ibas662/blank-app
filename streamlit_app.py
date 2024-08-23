b36c83e0dddceca777e6ec9d5b87cdd647286a2f
ibas662 <max.pen.662@gmail.com>
2024-08-23 18:45:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app3")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 pred")
+st.number_input('f1',min_value=1,max_value=10)
