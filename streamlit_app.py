a8eda92967edd2213af25aad080cf79ca74b6318
ibas662 <max.pen.662@gmail.com>
2024-08-23 18:49:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pickle
 
 st.title("🎈 pred")
-st.number_input('f1',min_value=1,max_value=10)
-st.number_input('f2',min_value=1,max_value=10)
-st.number_input('f3',min_value=1,max_value=100)
+f1=st.number_input('f1',min_value=1,max_value=10)
+f2=st.number_input('f2',min_value=1,max_value=10)
+f3=st.number_input('f3',min_value=1,max_value=100)
