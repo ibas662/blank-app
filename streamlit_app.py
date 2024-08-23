3a9b6d0124bc09f37bdeb300e540a75d5bdd11d9
ibas662 <max.pen.662@gmail.com>
2024-08-23 19:13:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ f3=st.number_input('f3',min_value=1,max_value=100)
 
 
 
-with open('model.pkl','rb') as file :
+with open('model(1).pkl','rb') as file :
   model = pickle.load(file)
 var = model.predict([[f1, f2, f3]])
