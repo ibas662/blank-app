6e12a6ddbbd0958b130137d912cefda80ebac34c
ibas662 <max.pen.662@gmail.com>
2024-08-23 19:03:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,9 @@ f1=st.number_input('f1',min_value=1,max_value=10)
 f2=st.number_input('f2',min_value=1,max_value=10)
 f3=st.number_input('f3',min_value=1,max_value=100)
 
+
+
 with open('model.pkl','rb') as file :
-  pickle.load(file)
+  model = pickle.load(file)
+var = model,predict([[f1,f2,f3]])
+st.write('co2 pred',var[0][0])
